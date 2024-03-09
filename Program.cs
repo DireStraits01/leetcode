c5a40fa4d32e6cd894cdaf33b835574ddeec9b2e
fhd <fhd@chanlerg.com>
2024-03-09 21:48:18 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace leetcode.tasks;
+internal class Program
+{
+   static void Main(){
+    
+    var n = new  SingleNumber();
+    n.Cww();
+
+   }
+}
