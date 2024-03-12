7f53efc965c0c42b141962ad0bc289e54e488d20
fhd <arsamak88@gmail.com>
2024-03-12 05:55:29 +0300

    remove element from array leetcode

--- Program.cs
+++ Program.cs
@@ -7,14 +7,7 @@ namespace leetcode.tasks;
 internal class Program
 {
    static void Main(){
-
-      var anim = new Animals();
-      var lion  = new Animal("Lion", "yellow", 300, "Raaaauuuu");
-      var cow  = new Animal("Cow", "white", 500, "mu");
-      var delphin  = new Animal("Delphin", "gray", 250, "piu");
-      Animal [] animalsArray = new Animal[]{lion, cow, delphin};
-      foreach(var animal in animalsArray){
-              animal.MakeSounds();
-      }
+   var el = new RemoveElement();
+   el.Action();
    }
 }
--- tasks/RemoveElement.cs
+++ tasks/RemoveElement.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,21 +8,17 @@ namespace leetcode.tasks
 {
     public class RemoveElement
     {
-        int[] nums = {1,2,3};
-        int val = 2;
+        int[] nums = {3,2,2,3};
+        int val = 3;
         int k = 0;
         public void Action(){
-               int[] nums2 = new int[nums.Length];
-               for (int i = 0; i < nums2.Length; i++){
-                foreach(var n in nums){
-                    if (n != val){
-                        nums2[i] = n;
-                    }
-                }
-               }
-               foreach(var nm in nums2){
-                System.Console.WriteLine(nm);
-               }
+           for(int i = 0; i  < nums.Length; i++){
+            if (nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+           }
+           
+            }
         }
     }
-}
